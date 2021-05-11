c707573b60e60ed615bec8833900f48ac6d69e15
Luis García Montañés <gamo.luis@gmail.com>
2021-05-11 13:32:33 +0200

    Change title for real this time

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my name is Luis';
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
+  
   
 }
